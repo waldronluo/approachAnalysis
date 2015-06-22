0d4bf8f552dc89fbf95ec8d11cfbd90055a4896f
waldronluo <waldronluo@Debian>
2015-06-22 11:57:31 +0800

    some

--- secondDerivative.m
+++ secondDerivative.m
@@ -3,20 +3,20 @@ axis = ['fx'; 'fy'; 'fz'; 'mx'; 'my'; 'mz'];
 
 cases = ls (dataRootPath);
    for i = 1:size(cases, 1)
+        label = caseLabel(cases(i, :))
         approachSig = load(strcat(dataRootPath, '/', cases(i, :)));
         approachSig = approachSig.approachSig;
         firstDerivative = derivative (approachSig);
         secondDerivative = derivative (firstDerivative);
-%        [[1:size(secondDerivative, 1)]' , abs(secondDerivative) > (mean(secondDerivative + 1 * std(secondDerivative)))]
         filter = abs(secondDerivative(:, 2:end)) > (mean(secondDerivative + 1 * std(secondDerivative))(:, 2:end));
         [x, y] = find(filter);
         begin = secondDerivative(x(1), 1);
-        approachSig((approachSig(:,1) >= begin),1);
-
-   %     for j = 1:size(secondDerivative, 1)
-   %     end
-        pause
+        features = extract_features(approachSig((approachSig(:,1) >= begin), :));
    end
+end
+
+function features = extract_features(contactSig)
+    features = [max(contactSig) - min(contactSig), mean(contactSig)] % This gives [time_range, amplitude for six axes]
 end
 
 function derivativeMatrix = derivative(sigMatrix)
@@ -34,6 +34,21 @@ derivativeMatrix = sigMatrix(2:end-1, 1);
             (sigMatrix(j+1, i) - sigMatrix(j-1, i)) / (sigMatrix(j+1, 1) - sigMatrix(j-1, 1))];
         end
         derivativeMatrix = [derivativeMatrix, tmpDerivative];
+    end
+end
+
+function label = caseLabel (caseName)
+
+if strncmp(caseName, 'success', 7)
+        label = 1;
+    elseif strncmp(caseName, 'FC', 2)
+        label = 2;
+    elseif strncmp(caseName, 'prelim', 6)
+        caseFlag = 0;
+    elseif strncmp(caseName, 'exp', 3)
+        caseFlag = 0;
+    else
+        caseFlag = 0;
     end
 end
 
